b68ad4d846071f8d475d6713c31da7100f9f7d16
tsubasasugawara <sugawara1740@gmail.com>
2023-05-09 17:27:36 +0000

    JSONの構造を修正

--- src/modules/.ipynb_checkpoints/parser-checkpoint.py
+++ src/modules/.ipynb_checkpoints/parser-checkpoint.py
@@ -12,7 +12,7 @@ def parse(in_dir_path, out_dir_path, artist):
     
     # 曲ごとの構文木のJSONファイルを出力
     for song_name in res["songs"]:
-        data = to_tree_map(artist, res["songs"][song_name])
+        data = to_map(artist, res["songs"][song_name])
 
         out_dir_path = util.put_slash_dir_path(out_dir_path)
         util.make_dir(out_dir_path)
@@ -44,43 +44,24 @@ def recur_tree(token):
     node = {"word": token.text, "number": token.i, "child_count": len(list(token.children)), "descendant_count": 0, "children": {}}
     descendant_count = 0
 
-    node["children"] = []
     for child in token.children:
+        node["children"] = {}
         node["children"].append(recur_tree(child))
-        descendant_count += node["children"][len(node["children"])-1]["descendant_count"]
         descendant_count += 1
+        descendant_count += node["children"][child.text]["descendant_count"]
 
     node["descendant_count"] = descendant_count
 
     return node
 
-# 根を結合する
-def join_roots(roots):
-    if len(roots) <= 1:
-        return roots
-
-    res = roots[0]
-    for i in range(1,len(roots)):
-        if res["child_count"] > roots[i]["child_count"]:
-            temp = roots[i]
-            temp["children"].append(res)
-            res = temp
-        else:
-            res["children"].append(roots[i])
-    return res
-
-
 # 文法構造をもとに、構文木のJSONファイルを作成する
-def to_tree_map(artist, song):
+def to_map(artist, song):
     data = {}
     for section in song:
-        data[section] = []
-        text = ""
+        data[section] = {}
         for sent in song[section].sents:
-            text = sent.text
-            data[section].append(recur_tree(sent.root))
-
-        data[section] = join_roots(data[section])
+            data[section][sent.text] = {}
+            data[section][sent.text][sent.root.text] = recur_tree(sent.root)
     
     return data
     
---
+++ src/modules/.ipynb_checkpoints/util-checkpoint.py
@@ -0,0 +1,45 @@
+import json
+import glob
+from pathlib import Path
+import os
+from os import path
+
+def read_json(file_path):
+    f = open(file_path, 'r', encoding="utf-8")
+    data = json.load(f)
+    f.close()
+    return data
+
+# dataは辞書型のデータ
+def output_json(file_path, data):
+    f = open(file_path, 'w+', encoding="utf-8")
+    json.dump(data, f, ensure_ascii=False, indent=2)
+    f.close()
+
+def put_slash_dir_path(dir_path):
+    if dir_path[-1] != "/":
+        dir_path = dir_path + "/"
+    return dir_path
+
+def get_file_list(dir_path):
+    if len(dir_path) <= 0:
+        return []
+    
+    path = put_slash_dir_path(dir_path) + "*.json"
+    
+    return glob.glob(path);
+
+# 拡張子などを取り除く
+def get_file_name(file_path):
+    return Path(file_path).stem
+
+def make_dir(dir_path):
+    if path.exists(dir_path) == False:
+        os.makedirs(dir_path)
+
+# 文字列が含まれるかどうか
+def contains(str_list, text):
+    res = False
+    for s in str_list:
+        res = res or s in text
+    return res
