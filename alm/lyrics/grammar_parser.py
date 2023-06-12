8ee6bed37ad91897446604104675ebd479929363
tsubasasugawara <sugawara1740@gmail.com>
2023-06-12 04:58:49 +0000

    GrammarParserを修正

--- alm/lyrics/grammar_parser.py
+++ alm/lyrics/grammar_parser.py
@@ -11,39 +11,62 @@ class GrammarParser:
         
         Attribute:
             nlp (spacy.Language): モデルを読み込んだもの
-            doc (spacy.Language): 文法解析の結果
-            tree (dict): 文法構造の構文解析木
-            words (list): 形態素解析で分割した単語のリスト
         """
 
         self.nlp = spacy.load(nlp_model_name)
-        self.doc = None
-        self.tree = None
-        self.words = []
 
-    def parse(self, lyrics: str) -> None:
+    def parse(self, lyrics: str) -> spacy.Language:
         """歌詞の文法構造を解析する
 
         Args:
             lyrics (str): 解析したい歌詞
+
+        Returns:
+            spacy.Language: 解析結果
         """
 
-        self.doc = self.nlp(lyrics)
+        return self.nlp(lyrics)
+     
+    def visualize(self, doc: spacy.Language) -> None:
+        """歌詞の文法構造を可視化する
 
-        for token in self.doc:
-            self.words.append(token.text)
+        Args:
+            doc (spacy.Language): parse関数での解析結果
+        """
+
+        displacy.render(doc, style='dep', jupyter=True, options={"compact": True, "distance": 90})
     
-    def to_tree_map(self) -> None:
-        """係り受け木を辞書型にする
+    def get_words_list(self, doc: spacy.Language) -> list:
+        """形態素解析によって分割された単語のリスト
+
+        Args:
+            doc (spacy.Language): _description_
+
+        Returns:
+            list: 単語のリスト
         """
 
-        if self.doc == None:
-            return
+        words_list = []
+        for token in doc:
+            words_list.append(token.text)
+
+        return words_list
+   
+    def to_tree_map(self, doc: spacy.Language) -> dict:
+        """構文解析木を辞書型にする
+
+        Args:
+            doc (spacy.Language): parse関数での解析結果
+
+        Returns:
+            dict: 構文解析木
+        """
 
         tree_list = []
-        for sent in self.doc.sents:
+        for sent in doc.sents:
             tree_list.append(self.__recur_tree(sent.root))
-        self.tree = self.__join_roots(tree_list)
+
+        return self.__join_roots(tree_list)
 
     def __recur_tree(self, token) -> dict:
         """再帰的に木を作成する
@@ -87,8 +110,3 @@ class GrammarParser:
                 res["children"].append(roots[i])
 
         return res
-    
-    def visualize(self) -> None:
-        """歌詞の文法構造を可視化する
-        """
-        displacy.render(self.doc, style='dep', jupyter=True, options={"compact": True, "distance": 90})
--- tests/lyrics/grammar_parser_test.py
+++ tests/lyrics/grammar_parser_test.py
@@ -3,11 +3,11 @@ from alm.utils import util
 
 def test_gramar_parser():
     parser = gp.GrammarParser("ja_ginza")
-    parser.parse("明日、今日よりも好きになれる。あふれる思いが止まらない。")
-    parser.to_tree_map()
+    doc = parser.parse("明日、今日よりも好きになれる。あふれる思いが止まらない。")
+    tree = parser.to_tree_map(doc)
 
-    print(parser.tree)
+    print(tree)
 
-    util.output_json("tests/test_file/grammar_parser_test.json", parser.tree)
+    util.output_json("tests/test_file/grammar_parser_test.json", tree)
 
 test_gramar_parser()
