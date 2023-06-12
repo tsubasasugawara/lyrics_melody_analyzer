0db3d7aa6de00ac7cb6cb4347c7bf0339b0fe73a
tsubasasugawara <sugawara1740@gmail.com>
2023-06-12 14:49:55 +0000

    lyrics_melody_matcherを修正

--- alm/comparator/lyrics_melody_matcher.py
+++ alm/comparator/lyrics_melody_matcher.py
@@ -1,80 +1,78 @@
 from ..lyrics import lyrics_extractor as LE
 
-class LyricsMelodyMatcher:
-    def __init__(self, lyrics_tree: dict, lyrics_notes_map: dict):
-        """歌詞とメロディを対応付けるクラス
+def match_word_notes(lyrics_tree: dict, lyrics_notes_map: dict) -> dict:
+    """単語と音符を対応付ける
 
-        Args:
-            lyrics_tree (dict): GrammarParserクラスで出力された歌詞の木
-            lyrics_notes_map (dict): LyricsExtractorクラスで出力された音符と歌詞の対応を示したリスト
+    Args:
+        lyrics_tree (dict): GrammarParserクラスで出力された歌詞の木
+        lyrics_notes_map (dict): LyricsExtractorクラスで出力された音符と歌詞の対応を示したリスト
+    
+    Returns:
+        dict: 音符を対応付けた構文解析木
+    """
 
-        Attribute:
-            lyrics_tree (dict): GrammarParserクラスで出力された歌詞の木
-            lyrics_notes_map (dict): LyricsExtractorクラスで出力された音符と歌詞の対応を示したリスト
-            words_notes_map (dict): 単語ごとに音符と対応付けされたマップ
-        """
+    words_notes_map = {}
+    words_notes_map = __explore_words_in_tree(lyrics_tree, words_notes_map)
 
-        self.lyrics_tree = lyrics_tree
-        self.lyrics_notes_map = lyrics_notes_map
-        self.words_notes_map = {}
+    key_list = sorted(words_notes_map)
+    index_words_map_key_list = 0
 
-        self.__explore_words_in_tree(self.lyrics_tree)
+    chars = ""
+    notes = []
+    for index_lyrics_map in range(0, len(lyrics_notes_map[LE.CHAR_NOTES_KEY])):
+        if index_words_map_key_list >= len(key_list):
+            break
 
-    def __explore_words_in_tree(self, tree: dict) -> None:
-        """構文解析木から単語を探索し、単語リストを作成する
+        key = key_list[index_words_map_key_list]
+        word = words_notes_map[key]["word"]
 
-        Args:
-            tree (dict): 構文解析木の部分木
-        """
+        chars = chars + lyrics_notes_map[LE.CHAR_NOTES_KEY][index_lyrics_map][LE.CHAR_KEY]
+        notes.extend(lyrics_notes_map[LE.CHAR_NOTES_KEY][index_lyrics_map][LE.NOTES_KEY])
+        notes = list(dict.fromkeys(notes))
 
-        word = tree["word"]
-        number = tree["number"]
+        if word == chars:
+            words_notes_map[key]["notes"] = notes
+            index_words_map_key_list += 1
 
-        self.words_notes_map[number] = {"word": word, "notes": []}
+            notes = []
+            chars = ""
+    
+    __match_notes_words_tree(lyrics_tree, words_notes_map)
+    return lyrics_tree
 
-        for child in tree["children"]:
-            self.__explore_words_in_tree(child)
+def __explore_words_in_tree(tree: dict, words_notes_map: dict) -> dict:
+    """構文解析木から単語を探索し、単語リストを作成する
 
-    def __match_notes_words_tree(self, tree: dict) -> None:
-        """単語の木に音符を対応付ける
+    Args:
+        tree (dict): 構文解析木の部分木
+        words_notes_map (dict): 分割された単語を格納する配列
+    
+    Returns:
+        dict: 分割された単語のリスト
+    """
 
-        Args:
-            tree (dict): 構文解析木の部分木
-        """
+    word = tree["word"]
+    number = tree["number"]
 
-        number = tree["number"]
-        tree["notes"] = self.words_notes_map[number]["notes"]
-        # notesとchildrenの順番を入れ替えるために、childrenを再度入れ直す
-        tree["children"] = tree.pop("children")
+    words_notes_map[number] = {"word": word, "notes": []}
 
-        for child in tree["children"]:
-            self.__match_notes_words_tree(child)
+    for child in tree["children"]:
+        __explore_words_in_tree(child, words_notes_map)
+    
+    return words_notes_map
 
-    def match_word_notes(self) -> None:
-        """単語と音符を対応付ける
-        """
+def __match_notes_words_tree(tree: dict, words_notes_map: dict) -> None:
+    """単語の木に音符を対応付ける
 
-        key_list = sorted(self.words_notes_map)
-        index_words_map_key_list = 0
+    Args:
+        tree (dict): 構文解析木の部分木
+        words_notes_map (dict): 単語ごとに音符と対応付けされたマップ
+    """
 
-        chars = ""
-        notes = []
-        for index_lyrics_map in range(0, len(self.lyrics_notes_map[LE.CHAR_NOTES_KEY])):
-            if index_words_map_key_list >= len(key_list):
-                break
+    number = tree["number"]
+    tree["notes"] = words_notes_map[number]["notes"]
+    # notesとchildrenの順番を入れ替えるために、childrenを再度入れ直す
+    tree["children"] = tree.pop("children")
 
-            key = key_list[index_words_map_key_list]
-            word = self.words_notes_map[key]["word"]
-
-            chars = chars + self.lyrics_notes_map[LE.CHAR_NOTES_KEY][index_lyrics_map][LE.CHAR_KEY]
-            notes.extend(self.lyrics_notes_map[LE.CHAR_NOTES_KEY][index_lyrics_map][LE.NOTES_KEY])
-            notes = list(dict.fromkeys(notes))
-
-            if word == chars:
-                self.words_notes_map[key]["notes"] = notes
-                index_words_map_key_list += 1
-
-                notes = []
-                chars = ""
-        
-        self.__match_notes_words_tree(self.lyrics_tree)
+    for child in tree["children"]:
+        __match_notes_words_tree(child, words_notes_map)
--- tests/comparator/lyrics_melody_matcher_test.py
+++ tests/comparator/lyrics_melody_matcher_test.py
@@ -4,19 +4,14 @@ from alm.lyrics import lyrics_extractor as LE
 from alm.utils import util
 
 def match_words_notes_test(file_path: str):
-    lyrics_notes_map = LE.abstract_lyrics(file_path)
+    lyrics_notes_map = LE.extract_lyrics(file_path)
 
     parser = GP.GrammarParser("ja_ginza")
     doc = parser.parse(lyrics_notes_map[LE.LYRICS_KEY])
     tree = parser.to_tree_map(doc)
 
-    matcher = LMM.LyricsMelodyMatcher(tree, lyrics_notes_map)
-    matcher.match_word_notes()
-    key_list = sorted(matcher.words_notes_map)
-    for key in key_list:
-        print(matcher.words_notes_map[key])
-
-    print(matcher.lyrics_tree)
+    lyrics_tree = LMM.match_word_notes(tree, lyrics_notes_map)
+    print(lyrics_tree)
 
 match_words_notes_test("tests/test_file/オレンジ/オレンジ_A1.xml")
 match_words_notes_test("tests/test_file/オレンジ/オレンジ_S1.xml")
