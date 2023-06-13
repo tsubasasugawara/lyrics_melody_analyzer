25da6bccb302b2c44d13f784ecdb23831bd8288f
tsubasasugawara <sugawara1740@gmail.com>
2023-06-13 02:23:42 +0000

    grammar_parserを修正

--- alm/lyrics/grammar_parser.py
+++ alm/lyrics/grammar_parser.py
@@ -1,5 +1,6 @@
 import spacy
 from spacy import displacy
+from ..utils import util
 
 class GrammarParser:
     def __init__(self, nlp_model_name: str):
@@ -107,5 +108,47 @@ class GrammarParser:
                 res = temp
             else:
                 res["children"].append(roots[i])
+
+        return res
+
+    #TODO: 句読点を追加すると歌詞の木に追加されてしまい、対応する音符が無いためエラーになる可能性あり
+    def add_punctuation(self, text: str) -> str:
+        """句読点を追加
+
+        Args:
+            text (str): 句読点を追加したい文字列
+
+        Returns:
+            str: 句読点を追加した文字列
+        """
+
+        doc = self.nlp(text)
+
+        res = ""
+        pre_token = None
+
+        for sent in doc.sents:
+            for token in sent:
+                
+                if token.text != "\n":
+                    res = res + token.text
+
+                if pre_token == None:
+                    pre_token = token
+                    continue
+
+                pre_morph = pre_token.morph.get("Inflection")
+
+                if token.text == "\n" and pre_token.text != ("!" or "?" or "！" or "？"):
+                    if (
+                        (len(pre_morph) > 0 and util.contains(["連用形", "連体形"], pre_morph[0]))
+                        or util.contains(["名詞", "間投詞", "副詞", "接続詞", "感嘆詞"], pre_token.tag_)
+                        or ("助詞" in pre_token.tag_ and "終助詞" not in pre_token.tag_)
+                        ):
+                        res = res + "、"
+                    else:
+                        res = res + "。"
+
+                pre_token = token
 
         return res
--- tests/lyrics/grammar_parser_test.py
+++ tests/lyrics/grammar_parser_test.py
@@ -9,3 +9,10 @@ def test_gramar_parser(lyrics: str):
     print(tree)
 
 test_gramar_parser("明日、今日よりも好きになれる。あふれる思いが止まらない。")
+
+def add_punctuation_test(lyrics: str):
+    parser = gp.GrammarParser("ja_ginza")
+    res = parser.add_punctuation(lyrics)
+    print(res)
+
+add_punctuation_test("明日、今日よりも好きになれる。あふれる思いが止まらない。")
