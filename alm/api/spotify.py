6e24bdb3b3200518fb38cc5c5bdc0c1625ca70b5
tsubasasugawara <sugawara1740@gmail.com>
2023-07-23 06:40:38 +0000

    popularityを取得

--- alm/api/spotify.py
+++ alm/api/spotify.py
@@ -1,6 +1,7 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import os
+import time
 
 class Track:
     def __init__(self, name: str, popularity: str) -> None:
@@ -13,6 +14,20 @@ class Track:
 
         self.name = name
         self.popularity = popularity
+
+def get_popularities(track_id_list: list) -> list:
+    client_id = os.environ["SPOTIFY_CLIENT_ID"]
+    client_secret = os.environ["SPOTIFY_SECRET"]
+    client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
+
+    res = []
+    for track_id in track_id_list:
+        track = get_track(track_id, client_credentials_manager)
+        time.sleep(3)
+        if type(track) is dict:
+            res.append([track["name"], track["popularity"]])
+    
+    return res
 
 def get_popularity(track_id: str) -> (Track | None):
     """人気度を取得する
