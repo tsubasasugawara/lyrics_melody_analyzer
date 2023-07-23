4b988adb49c60c0ef02ab7b9f851b121643e04a3
tsubasasugawara <sugawara1740@gmail.com>
2023-07-23 06:15:00 +0000

    spotifyのpopularityを取得する処理を実装

---
+++ alm/api/spotify.py
@@ -0,0 +1,54 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import os
+
+class Track:
+    def __init__(self, name: str, popularity: str) -> None:
+        """トラック情報
+
+        Args:
+            name (str): トラック名
+            popularity (str): 人気度
+        """
+
+        self.name = name
+        self.popularity = popularity
+
+def get_popularity(track_id: str) -> (Track | None):
+    """人気度を取得する
+
+    Args:
+        track_id (str): トラックID
+
+    Returns:
+        (Track | None): トラック情報を取得できたらTrack型のデータを返す
+    """
+
+    client_id = os.environ["SPOTIFY_CLIENT_ID"]
+    client_secret = os.environ["SPOTIFY_SECRET"]
+    client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
+    
+    track = get_track(track_id, client_credentials_manager)
+
+    if type(track) is dict:
+        return Track(track["name"], track["popularity"])
+    else:
+        return None
+
+def get_track(track_id: str, client_credentials_manager: SpotifyClientCredentials) -> (dict | None):
+    """トラック情報を取得する
+
+    Args:
+        track_id (str): トラックID
+
+    Returns:
+        (dict | None): トラック情報を取得できたときに辞書型配列を返す
+    """
+
+    spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager, language="ja")
+
+    try:
+        res = spotify.track(track_id, market="JP")
+        return res
+    except:
+        return None
