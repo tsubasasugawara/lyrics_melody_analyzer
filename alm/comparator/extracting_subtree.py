4384d4de529fad43f68d37c9bbced091a2230692
tsubasasugawara <sugawara1740@gmail.com>
2023-09-27 09:08:14 +0900

    レベル2までの木構造の部分木を抽出する処理を実装

--- alm/comparator/extracting_subtree.py
+++ alm/comparator/extracting_subtree.py
@@ -1,6 +1,6 @@
-from alm.node import node
+from alm.node import node as nd
 
-def extract_parent_child(node: node.Node) -> list:
+def extract_parent_child(node: nd.Node) -> list:
     res = []
     for child in node.children:
         res.append({"id": node.id, "child": child.id})
@@ -8,3 +8,17 @@ def extract_parent_child(node: node.Node) -> list:
     
     return res
 
+def extract_subtree(node: nd.Node):
+    if node.end:
+        return []
+    
+    res = []
+    n = len(node.children)
+    for i in range(1, 2**n):
+        subtree = nd.Node(node.id, [], False)
+        for j in range(n):
+            if i >> j & 1:
+                subtree.children.append(node.children[j])
+        res.append(subtree)
+    
+    return res
--- tests/comparator/extracting_subtree_test.py
+++ tests/comparator/extracting_subtree_test.py
@@ -1,6 +1,7 @@
 from alm.comparator import *
 from alm.lyrics import *
 from alm.melody import *
+from alm.node import node
 import pprint
 
 def extracting_parent_child_test(mscx_path: str, tstree_path: str):
@@ -26,4 +27,38 @@ def extracting_parent_child_test(mscx_path: str, tstree_path: str):
     pprint.pprint(lyrics_subtree_list)
     pprint.pprint(tstree_subtree_list)
 
-extracting_parent_child_test("xmls/pop/BE_FREE_A.xml", "xmls/pop_tstree/BE_FREE_A_TS.xml")
+def extract_subtrees_test():
+    root = node.Node(
+        1,
+        [
+            node.Node(
+                2,
+                [],
+                True
+            ),
+            node.Node(
+                3,
+                [],
+                True
+            ),
+            node.Node(
+                4,
+                [],
+                True
+            ),
+            node.Node(
+                5,
+                [],
+                True
+            ),
+        ],
+        False
+    )
+
+    res = extracting_subtree.extract_subtree(root)
+
+    for item in res:
+        pprint.pprint(item.to_dict())
+
+extracting_parent_child_test("xmls/mscx/BE_FREE_A.xml", "xmls/tstree/BE_FREE_A_TS.xml")
+extract_subtrees_test()
