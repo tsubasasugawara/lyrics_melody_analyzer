418b38ddd8e4b6aead02f6b51a27877af76dde35
tsubasasugawara <sugawara1740@gmail.com>
2023-10-28 14:38:24 +0900

    コマンドを修正

--- alm/main.py
+++ alm/main.py
@@ -4,7 +4,11 @@ from alm.lyrics import grammar_parser as gp
 from alm.comparator import tree_similarity_calculator as tsc
 
 def main():
-    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser = argparse.ArgumentParser(description='This program calc word match rates and tree similarities.')
+    parser.add_argument(
+        'mode',
+        help="'word-match-rate' or 'tree-similarity'"
+    )
     parser.add_argument(
         'mscx_path',
         help='MusicXML Path'
@@ -15,8 +19,11 @@ def main():
     )
     args = parser.parse_args()
 
-    similarity = tsc.calc_tree_similarity(args.mscx_path, args.tstx_path, gp.GrammarParser("ja_ginza"))
-    similarity.print()
+    if args.mode == 'tree-similarity':
+        similarity = tsc.calc_tree_similarity(args.mscx_path, args.tstx_path, gp.GrammarParser("ja_ginza"))
+        similarity.print()
+    #elif args.mode == 'word-match-rate':
+
 
 if __name__ == "__main__":
     sys.exit(main())
