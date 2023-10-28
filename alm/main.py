7aa53efb47d59ff70deed2947d97f00a0f8c6bd8
tsubasasugawara <sugawara1740@gmail.com>
2023-10-28 01:53:21 +0000

    コマンド作成の途中

--- alm/main.py
+++ alm/main.py
@@ -1,63 +1,30 @@
 import sys
+import argparse
 from alm.lyrics import *
 from alm.melody import *
 from alm.comparator import *
 from alm.utils import io
 
-POP_DIR = "xmls/pop/"
-POP_TSTREE_DIR = "xmls/pop_tstree/"
-UNPOP_DIR = "xmls/unpop/"
-UNPOP_TSTREE_DIR = "xmls/unpop_tstree/"
-GINZA = "ja_ginza"
+def main():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument(
+        'integers',
+        metavar='N',
+        type=int,
+        nargs='+',
+        help='an integer for the accumulator'
+    )
+    parser.add_argument(
+        '--sum',
+        action='store_true',
+        help='sum the integers (default: find the max)'
+    )
 
-#TODO: 作り直し
-def calc_word_matched_rate(mscx_list: list, parser: grammar_parser.GrammarParser, word_matched_rate_list:list = []) -> list:
-    res = word_matched_rate_list
-
-    for mscx_path in sorted(mscx_list):
-        ts_path = mscx_path.replace("pop", "pop_tstree")
-        ts_path = ts_path.replace(".xml", "_TS.xml")
-
-        lyrics_notes_dict = lyrics_extractor.extract_lyrics(mscx_path)
-        doc = parser.parse(lyrics_notes_dict[lyrics_extractor.LYRICS_KEY])
-        lyrics_tree = parser.to_tree(doc)
-        words_notes_dict = {}
-        associating_lyrics_melody.explore_words_in_tree(lyrics_tree, words_notes_dict)
-        words_list = associating_lyrics_melody.associate_word_list_notes(words_notes_dict, lyrics_notes_dict)
-
-        melody_tree = time_span_tree.tstree_xml_2_struct(ts_path)
-
-        rate = word_matched_rate_calculator.calc_word_matched_rate(words_list, melody_tree)
-        rate .section_name = io.get_file_name(mscx_path)
-
-        res.append([rate.section_name, rate.words_number, rate.match_words_number, rate.words_number - rate.match_words_number, rate.match_rate])
-    
-    return res
-
-def output_word_matched_rate_csv() -> None:
-    parser = grammar_parser.GrammarParser(GINZA)
-
-    pop_xmls = io.get_file_list(POP_DIR, io.XML)
-    res = calc_word_matched_rate(pop_xmls, parser)
-    
-    unpop_xmls = io.get_file_list(UNPOP_DIR, io.XML)
-    res = calc_word_matched_rate(unpop_xmls, parser, res)
-
-    io.output_csv("./word_matched_rate.csv", ["セクション名", "単語数", "一致した単語数", "一致しなかった単語数", "一致率"], res)
-
-    return res
-
-def main(argv):
-    if len(argv) <= 1:
-        return
-
-    if argv[1] == "word-match-rate":
-        if len(argv) >= 2:
-            res = calc_word_matched_rate([argv[2]], grammar_parser.GrammarParser(GINZA))
-            data = res[0]
-            print(f"{data[0]}\n単語数：　　　　　　　{data[1]}\n一致した単語数：　　　{data[2]}\n一致しなかった単語数：{data[3]}\n一致率：　　　　　　　{data[4]}")
-        else:
-            res = output_word_matched_rate_csv()
+    args = parser.parse_args()
+    if args.sum:
+        print(sum(args.integers))
+    else:
+        print(max(args.integers))
 
 if __name__ == "__main__":
-    sys.exit(main(sys.argv))
+    sys.exit(main())
